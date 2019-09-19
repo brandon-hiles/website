c352cd19a0fb2beb6a2517fb979c5e47712caa71
brandon.hiles <brandon.hiles@brandonhiles.com>
2019-09-19 15:01:15 -0400

    Updating main.js

--- v2/main.js
+++ v2/main.js
@@ -1,23 +1,23 @@
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/" // Use Proxy URL to get around CORS security
 const MEDIUM_URL = 'https://medium.com/feed/@brandon.j.hiles';
 
-
 let parser = new RSSParser();
 parser.parseURL(CORS_PROXY + MEDIUM_URL, function(err, feed) {
-  console.log(feed)
-  feed.items.forEach(function(entry) {
-    console.log(entry)
-    let link = entry.link;
-    $("#blog-link-1").attr("href", link);
-    $("#blog-link-2").attr("href", link);
-    $("#blog-title-1").text(entry.title);
-    $("#blog-title-2").text(entry.title);
-    $.get(CORS_PROXY + link, response => {
+  length = feed.items.length
+  feed.items.forEach(function(entry,index) {
+    $.get(CORS_PROXY + entry.link, response => {
       let img = $(response).find('img')['1'];
       let imgSrc = $(img).attr('src').split("?")[0].replace('/max/60', '/max/400');
-
+    if (index == length-2) { // Second most recent article
+      $("#blog-link-1").attr("href", entry.link);
+      $("#blog-title-1").text(entry.title);
       $("#blog-img-1").attr('style', '--url: url(' + imgSrc + ')');
+    }
+    if (index == length-1) { // Most recent article
+      $("#blog-link-2").attr("href", entry.link);
+      $("#blog-title-2").text(entry.title);
       $("#blog-img-2").attr('style', '--url: url(' + imgSrc + ')');
+    }
     })
   })
 })
