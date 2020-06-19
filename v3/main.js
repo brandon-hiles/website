2860ad9ba65fe6afcbb32c0867302d13480a4ccf
brandon.hiles <brandon.hiles@brandonhiles.com>
2020-06-18 20:19:54 -0400

    Updating main.js

--- v3/main.js
+++ v3/main.js
@@ -7,13 +7,14 @@ parser.parseURL(CORS_PROXY + MEDIUM_URL, function(err, feed) {
   feed.items.forEach(function(entry,index) {
     $.get(CORS_PROXY + entry.link, response => {
       let img = $(response).find('img')['1'];
+      console.log(entry.title)
       let imgSrc = $(img).attr('src').split("?")[0].replace('/max/60', '/max/400');
-    if (index == 0) { // Second most recent article
+    if (entry.title === 'Data Mining with Python3' ) { // Second most recent article
       $("#blog-link-1").attr("href", entry.link);
       $("#blog-title-1").text(entry.title);
       $("#blog-img-1").attr('style', '--url: url(' + imgSrc + ')');
     }
-    if (index == 1) { // Most recent article
+    if (entry.title === 'History of Asynchronous Javascript') { // Most recent article
       $("#blog-link-2").attr("href", entry.link);
       $("#blog-title-2").text(entry.title);
       $("#blog-img-2").attr('style', '--url: url(' + imgSrc + ')');
