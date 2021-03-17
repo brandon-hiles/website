eb0ad1259fa661ef259e4e6bb195f03838eb9062
brandon.hiles <brandon.hiles@brandonhiles.com>
2021-03-16 23:37:42 -0400

    Changing proxy server location

--- v3/main.js
+++ v3/main.js
@@ -1,24 +1,19 @@
-const CORS_PROXY = "https://cors-anywhere.herokuapp.com/" // Use Proxy URL to get around CORS security
-const MEDIUM_URL = 'https://medium.com/feed/@brandon.j.hiles';
+// Customized Proxy Server to avoid the annoying CORS Issues.
+const PROXY_URL = "https://serene-roentgen-3287a4.netlify.app/.netlify/functions/api/blog"
 
 let parser = new RSSParser();
-parser.parseURL(CORS_PROXY + MEDIUM_URL, function(err, feed) {
+parser.parseURL(PROXY_URL, function(err, feed) {
   length = feed.items.length
   feed.items.forEach(function(entry,index) {
-    $.get(CORS_PROXY + entry.link, response => {
-      let img = $(response).find('img')['1'];
-      console.log(entry.title)
-      let imgSrc = $(img).attr('src').split("?")[0].replace('/max/60', '/max/400');
-    if (entry.title === 'Data Mining with Python3' ) { // Second most recent article
+    if (entry.title === 'Data Mining with Python3' ) { // Most recent article
       $("#blog-link-1").attr("href", entry.link);
       $("#blog-title-1").text(entry.title);
-      $("#blog-img-1").attr('style', '--url: url(' + imgSrc + ')');
+      $("#blog-img-1").attr('style', '--url: url(https://miro.medium.com/max/400/0*mGjwfxQlhPu2_zih)');
     }
-    if (entry.title === 'History of Asynchronous Javascript') { // Most recent article
+    if (entry.title === 'History of Asynchronous Javascript') { // Second Most recent article
       $("#blog-link-2").attr("href", entry.link);
       $("#blog-title-2").text(entry.title);
-      $("#blog-img-2").attr('style', '--url: url(' + imgSrc + ')');
+      $("#blog-img-2").attr('style', '--url: url(https://miro.medium.com/max/400/1*bhWrEhpSdKU0BvmuaIXjzw.png)');
     }
-    })
   })
 })
